6fb2021e6d95a117a665d534ce4ffd7b009aa70a
Pavel Strelchenia <pivlik5608@gmail.com>
2026-02-17 18:43:47 +0300

    Added the main calculation function

--- main.js
+++ main.js
@@ -24,4 +24,28 @@ function division(a, b) {
     return a / b;
 }
 
-//
+// Variables for storing operation components
+
+let firstNumber = null;
+let secondNumber = null;
+let operator = undefined;
+
+const plus = "+";
+const minus = "-";
+const mult = "*";
+const divis = "/";
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case plus:
+            return sum(a, b);
+        case minus:
+            return sub(a, b);
+        case mult:
+            return multi(a, b);
+        case divis:
+            return b === 0 ? "ERROR" : division(a, b);
+        default:
+            return "ERROR";
+    }
+}
