327404245277b3f2665bb219a604eb6cda890fcf
Pavel Strelchenia <pivlik5608@gmail.com>
2026-02-17 17:08:03 +0300

    Added basic calculation functions

---
+++ main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// Addition
+
+function sum(a, b) {
+    return a + b;
+}
+
+// Subtraction
+
+function sub(a, b) {
+    return a - b;
+}
+
+// Multiplication
+
+function multi(a, b) {
+    return a * b;
+}
+
+// Division
+
+function division(a, b) {
+    return a / b;
+}
+
+//
